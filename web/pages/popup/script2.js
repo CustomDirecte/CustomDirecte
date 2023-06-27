501d412f64deab3d65aae46b28c11c1c3bb4dbc3
Bastian-Noel <71983581+Bastian-Noel@users.noreply.github.com>
2023-06-27 17:22:38 +0200

    Big Update In preparation

--- web/pages/popup/script2.js
+++ web/pages/popup/script2.js
@@ -1,4 +1,20 @@
-document.querySelectorAll("[groupButton]").forEach((ele) => ele.addEventListener('click', () => {
-  document.querySelectorAll("[selected]").forEach((selectedEle) => selectedEle.removeAttribute('selected'));
-  ele.toggleAttribute("selected");
-}));
+/* GENERATION */
+
+
+
+/* USEFULL */
+groupButton = document.querySelectorAll("[groupButton]");
+group = document.querySelectorAll("[group]");
+
+groupButton.forEach((ele) =>
+  ele.addEventListener("click", () => {
+    document.querySelector("[selected]").removeAttribute("selected");
+    ele.toggleAttribute("selected");
+
+    document.querySelectorAll("[group]").forEach((selectedEle) => {
+      selectedEle.toggleAttribute("hide", true);
+      selectedEle.scrollTo(0, 0);
+    });
+    document.querySelector('[group="' + ele.getAttribute("groupButton") + '"]').removeAttribute("hide");
+  })
+);
